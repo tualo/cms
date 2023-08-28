c9e966e32f3229e1d7448aae193aac6de3ee9726
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-08-28 10:34:05 +0200

    add default on get method

--- src/CMSMiddleware/Session.php
+++ src/CMSMiddleware/Session.php
@@ -4,11 +4,12 @@ use Tualo\Office\Basic\TualoApplication as App;
 
 class Session {
     public function db() { return App::get('session')->getDB(); }
-    public function get($key):mixed { 
-        return $_SESSION[$key]; 
+    public function get($key,$default=false):mixed { 
+        return isset($_SESSION[$key])?$_SESSION[$key]:$default; 
     }
     public function set($key,$value):Session { 
         @session_start();
+
         $_SESSION[$key]=$value; 
         return $this;
     }
