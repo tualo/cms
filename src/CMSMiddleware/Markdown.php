f98d642baf1a677b78bea77e17b903282d6f9b45
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-20 13:07:35 +0200

    add markdown extra

--- src/CMSMiddleware/Markdown.php
+++ src/CMSMiddleware/Markdown.php
@@ -5,6 +5,17 @@ use Michelf\MarkdownExtra;
 use Parsedown;
 
 class Markdown {
+
+    
+    public static function markdownExtrafn():mixed{
+        return function(string|null $markdownText):string{
+            if(is_null($markdownText))  return ''; 
+            $result = MarkdownExtra::defaultTransform( $markdownText );
+            if (strpos($result,"<p>")===0) $result = substr( $result ,3,-5);
+            return $result;
+        };
+    }
+
     public static function markdownfn():mixed{
         return function(string|null $markdownText):string{
             $Parsedown = new Parsedown();
@@ -15,11 +26,13 @@ class Markdown {
         };
     }
 
+
     public function db() { return App::get('session')->getDB(); }
     
     
 
     public static function run(&$request,&$result){
         $result['markdown']= self::markdownfn();
+        $result['markdownExtra']= self::markdownExtrafn();
     }
 }
