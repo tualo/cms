c4df79a9a70a4b8c7a06e74896b32eb3b1053f93
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2024-12-06 07:57:27 +0100

    utf8

--- src/sql/install/tualocms_attribute.sql
+++ src/sql/install/tualocms_attribute.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS `tualocms_attribute` (
   `tualocms_attribute` varchar(36) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`tualocms_attribute`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ;
--- src/sql/install/tualocms_middleware.sql
+++ src/sql/install/tualocms_middleware.sql
@@ -2,4 +2,4 @@ DELIMITER ;
 CREATE TABLE IF NOT EXISTS `tualocms_middleware` (
   `tualocms_middleware` varchar(50) NOT NULL,
   PRIMARY KEY (`tualocms_middleware`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ;
