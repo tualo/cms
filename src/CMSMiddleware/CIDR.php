76b292c9e56fa112c1f5e9d57ef2dfacbbdd24e3
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-06-11 13:23:57 +0200

    fix key

--- src/CMSMiddleware/CIDR.php
+++ src/CMSMiddleware/CIDR.php
@@ -250,7 +250,7 @@ class CIDR
         return $range;
     }
 
-    public static function getIP($keys = ['X-DDOSPROXY', 'HTTP_X_FORWARDED_FOR', 'HTTP_CLIENT_IP', 'REMOTE_ADDR'])
+    public static function getIP($keys = ['HTTP_X_DDOSPROXY', 'HTTP_X_FORWARDED_FOR', 'HTTP_CLIENT_IP', 'REMOTE_ADDR'])
     {
         foreach ($keys as $key) {
             if (isset($_SERVER[$key]) && self::validIP($_SERVER[$key])) {
@@ -262,7 +262,7 @@ class CIDR
 
 
 
-    public function ip($keys =  ['X-DDOSPROXY', 'HTTP_X_FORWARDED_FOR', 'HTTP_CLIENT_IP', 'REMOTE_ADDR']): mixed
+    public function ip($keys =  ['HTTP_X_DDOSPROXY', 'HTTP_X_FORWARDED_FOR', 'HTTP_CLIENT_IP', 'REMOTE_ADDR']): mixed
     {
         return CIDR::getIP($keys);
     }
