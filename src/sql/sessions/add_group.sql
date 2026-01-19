2736cacfcce2ed91e30083ad0e758547b8758b2b
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-01-19 15:58:55 +0100

    Version bump to 1.2.29

--- src/sql/sessions/add_group.sql
+++ src/sql/sessions/add_group.sql
@@ -2,5 +2,12 @@ delimiter ;
 
 insert ignore into SESSIONDB.macc_groups (name,aktiv) values ('public-cms',1);
 
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`role`,`table_name`,`write`) VALUES ('0','0','1','public-cms','view_load_tualocms_page','0') ; 
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`role`,`table_name`,`write`) VALUES ('0','0','1','public-cms','view_load_tualocms_page_csp','0') ; 
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`role`,`table_name`,`write`) VALUES ('0','0','1','public-cms','view_load_tualocms_page_headers','0') ; 
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`role`,`table_name`,`write`) VALUES ('0','0','1','public-cms','view_load_tualocms_page_permission_policy','0') ; 
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`role`,`table_name`,`write`) VALUES ('0','0','1','public-cms','tualocms_additional_headers','0') ; 
+ 
 insert ignore into route_scopes(scope) values ('cms.page');
 insert ignore into route_scopes_permissions (scope,`group`,allowed) values ('cms.page', 'public-cms', 1);
