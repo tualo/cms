f3bb2009c7c8fa4b0f1ec87f1b8ee89ad8cca765
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-01-19 09:19:15 +0100

    Version bump to 1.2.21

--- src/Commands/SetupCMSApache.php
+++ src/Commands/SetupCMSApache.php
@@ -89,7 +89,7 @@ EOT;
             while ($args->getOpt('silent', false) || in_array($line = readline(implode("\n", $prompt)), ['yes', 'y', 'n', 'no', 'c'])) {
                 if ($line == 'c') exit();
                 if ($line == 'y') {
-                    PostCheck::formatPrint(['blue'], "creating ../.htaccess\n");
+                    PostCheck::formatPrint(['blue'], "creating ../.htaccess");
                     file_put_contents(
                         dirname((string)App::get('basePath')) . '/.htaccess',
                         DataRenderer::renderTemplate(
